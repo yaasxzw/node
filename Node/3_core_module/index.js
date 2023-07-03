8fa92d8125e7cdfdc89dc9e6c8a8797215b1db07
Yasmim Guedes <110118206+yaasxzw@users.noreply.github.com>
2023-07-03 16:35:12 -0300

    Delete Node directory

--- Node/3_core_module/index.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-const exetension = path.extname('Arquivo.java')
-
-console.log(exetension)
--- Node/5_modulo_externo/index.js
+++
@@ -1,9 +0,0 @@
-const minimist = require('minimist')
-// console.log(minimist)
-//node index.js --nome=yasmim --idade=17
-const args = minimist(process.argv.slice(2))
-console.log(args)
-const nome = args['nome']
-const idade = args['idade']
-
-console.log(`o nome ${nome} e idade ${idade}`)
